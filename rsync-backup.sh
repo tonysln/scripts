f4ac3f2c08c29a0164de772b2a88a0109e8e22b2
Anton Slavin <slavinant@gmail.com>
2025-08-29 20:34:51 +0300

    Cleanup and misc fixups

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 
-folders="docs dev media uni work"
+folders="slavin"
+backup_path="/media/anton/Storage"
 
 for folder in $folders;
 do
-	rsync -abvPh --delete-excluded --exclude-from="rsync-exclude.txt" --backup-dir="/Volumes/untitled/Backup/${folder}_$(date +%Y-%m-%d)" --delete "/Users/tony/${folder}/" "/Volumes/untitled/${folder}/" 
+	rsync -abvPht --no-links --perms --no-owner --no-group --delete-excluded --exclude-from="rsync-exclude.txt" --backup-dir="${backup_path}/Backup/${folder}_$(date +%Y-%m-%d)" --delete "/home/anton/${folder}/" "${backup_path}/${folder}/"
 done
 
