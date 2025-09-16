35905355edcec826dac8710be2a524202e91a02c
Anton Slavin <slavinant@gmail.com>
2025-09-17 01:08:52 +0300

    Almost correct faster BWT based on suffix arrays

--- bwt.py
+++ bwt.py
@@ -13,7 +13,7 @@ def naive_BWT(s, start='^', end='$'):
     return ''.join([c[-1] for c in rr])
 
 
-def inverse_naive_BWT(s, start='^', end='$'):
+def naive_IBWT(s, start='^', end='$'):
     ls = len(s)
     r = ['']*ls
     for i in range(ls):
@@ -27,15 +27,99 @@ def inverse_naive_BWT(s, start='^', end='$'):
 
  
 
-print(inverse_naive_BWT(naive_BWT(ts)))
-
-def BWT(s, eof='\0'):
-    s = s + eof
-    return s
+print(naive_IBWT(naive_BWT(ts)))
 
 
-def inverse_BWT(s, eof='\0'):
-    return s
+def sort_cyclic_shifts(s):
+    # https://cp-algorithms.com/string/suffix-array.html
+    n = len(s)
+    ab = 256
+
+    p = [None]*n
+    c = [None]*n
+    cnt = [0]*max(ab, n)
+
+    for i in range(0, n, 1):
+        cnt[ord(s[i])] += 1
+
+    for i in range(1, ab, 1):
+        cnt[i] += cnt[i-1]
+
+    for i in range(0, n, 1):
+        cnt[ord(s[i])] -= 1
+        p[cnt[ord(s[i])]] = i
+
+    c[p[0]] = 0
+    cc = 1
+    for i in range(1, n, 1):
+        if s[p[i]] != s[p[i-1]]:
+            cc += 1
+
+        c[p[i]] = cc - 1
+
+    pn = [None]*n
+    cn = [None]*n
+
+    h = 0
+    while (1 << h) < n:
+        h += 1
+
+        for i in range(0, n, 1):
+            pn[i] = p[i] - (1 << h)
+            if pn[i] < 0:
+                pn[i] += n
+
+        cnt[0:cc] = [0]*cc
+
+        for i in range(0, n, 1):
+            cnt[c[pn[i]]] += 1
+
+        for i in range(1, cc, 1):
+            cnt[i] += cnt[i-1]
+
+        for i in range(n-1, -1, -1):
+            cnt[c[pn[i]]] -= 1
+            p[cnt[c[pn[i]]]] = pn[i]
+
+        cn[p[0]] = 0
+        cc = 1
+
+        for i in range(1, n, 1):
+            cur = (c[p[i]], c[(p[i] + (1 << h)) % n])
+            prev = (c[p[i-1]], c[(p[i-1] + (1 << h)) % n])
+
+            if cur != prev:
+                cc += 1
+
+            cn[p[i]] = cc-1
+
+        c[:],cn[:] = cn[:],c[:]
+
+    return p
 
 
-print(inverse_BWT(BWT(ts)))
+def suffix_array(s, eof='$'):
+    # s += eof
+    ss = sort_cyclic_shifts(s)
+    return ss[1:]
+
+
+def BWT(s, eof='$'):
+    # https://www.csbio.unc.edu/mcmillan/Comp555S18/Lecture13.pdf
+    s += eof
+    return ''.join(s[i-1] for i in suffix_array(s))
+
+
+def IBWT(s, eof='$'):
+    # https://www.csbio.unc.edu/mcmillan/Comp555S18/Lecture13.pdf
+    table = ['' for c in s]
+    for j in range(len(s)):
+        table = sorted([c + table[i] for i,c in enumerate(s)])
+
+    return table[s.index(eof)]
+
+
+a = BWT(ts)
+# a = BWT('mississippi')
+print(a)
+print(IBWT(a))
