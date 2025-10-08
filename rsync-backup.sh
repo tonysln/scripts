9d5cffe71f2a8dc8025b7efd4350dd0857f88598
Anton Slavin <slavinant@gmail.com>
2025-10-08 23:52:29 +0300

    rsync backup script fixes

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 
-folders="slavin"
-backup_path="/media/anton/Storage"
+user="user"
+folders="dev docs media photos uni videos work"
+backup_path="/media/user/Storage"
 
 for folder in $folders;
 do
-	rsync -abvPht --no-links --perms --no-owner --no-group --delete-excluded --exclude-from="rsync-exclude.txt" --backup-dir="${backup_path}/Backup/${folder}_$(date +%Y-%m-%d)" --delete "/home/anton/${folder}/" "${backup_path}/${folder}/"
+	rsync -abvPht --no-links --perms --no-owner --no-group --delete-excluded --exclude-from="rsync-exclude.txt" --backup-dir="${backup_path}/Backup/${folder}_$(date +%Y-%m-%d)" --delete "/home/${user}/${folder}/" "${backup_path}/${folder}/"
 done
 
