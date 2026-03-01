f3a5115ee75066a00eeda4397bd2b309827fb669
Anton Slavin <slavinant@gmail.com>
2026-03-01 14:05:29 +0200

    Folder structure changes

--- rsync/rsync-backup.sh
+++ rsync/rsync-backup.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-
 user="user"
-folders="dev docs media photos uni videos work"
+folders="dev docs media uni work"
 # backup_path="/mnt/backup/@"
 # backup_path="/run/media/veracrypt1"
 backup_path=$1
 
-if [ -z "$backup_path" ]
-then
-   echo "Backup path not set!";
-   exit 1
+if [ -z "$backup_path" ]; then
+  echo "Backup path not set!"
+  exit 1
 fi
 
-
-for folder in $folders;
-do
-	rsync -abvPht --no-links --perms --no-owner --no-group --delete-excluded --exclude-from="rsync-exclude.txt" --backup-dir="${backup_path}/backup/${folder}_$(date +%Y-%m-%d)" --delete "/home/${user}/${folder}/" "${backup_path}/${folder}/"
+for folder in $folders; do
+  rsync -abvPht --no-links --perms --no-owner --no-group --delete-excluded --exclude-from="rsync-exclude.txt" --backup-dir="${backup_path}/backup/${folder}_$(date +%Y-%m-%d)" --delete "/home/${user}/${folder}/" "${backup_path}/${folder}/"
 done
-
